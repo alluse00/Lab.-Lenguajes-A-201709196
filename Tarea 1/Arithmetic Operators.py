2e91f4c6e94f4293ff50e65c923b0a78cf72da4e
Allan J. Rafael <allanmorales05@gmail.com>
2021-02-05 13:13:44 -0600

    Arithmetic Operators

---
+++ Tarea 1/Arithmetic Operators.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    suma = a + b
+    resta = a - b
+    multi = a*b
+    print(suma)
+    print(resta)
+    print(multi)
