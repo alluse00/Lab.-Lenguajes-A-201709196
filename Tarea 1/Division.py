d18c36beddff8d65419c4bc85e74843ef7020b7f
Allan J. Rafael <allanmorales05@gmail.com>
2021-02-05 13:17:06 -0600

    Division

---
+++ Tarea 1/Division.py
@@ -0,0 +1,9 @@
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+sinde = a/b
+conde = float(a)/float(b)
+print(int(sinde))
+print(conde)
