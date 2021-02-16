94fbf088409f400a4331a1bf5f9ec6fc2a42cf0f
Allan J. Rafael <allanmorales05@gmail.com>
2021-02-16 09:14:45 -0600

    Practica 2.0

--- Practica1/main.py
+++ Practica1/main.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 
 cm = []
-
 def abrirDoc():
     root = Tk()
     name = askopenfilename()
@@ -60,32 +59,163 @@ def Vacio(a):
     else:
         return True
 def Ordenados():
-    print("****Listas Ordenadas****")
+    print("******Listas Ordenadas******")
     for linea in cm:
         for elemento in linea:
             if elemento == "ORDENAR":
-                numero = QuitarComa(linea[1])
                 nombreLinea = linea[0]
+                numero = QuitarComa(linea[1])
                 num = ','.join(sorted(numero))
                 print(nombreLinea + ": ORDENADOS = " + num, end="\n")
 def Buscados():
-    print("****Listas Buscadas***")
+    print("*******Listas Buscadas******")
     for linea in cm:
         for elemento in linea:
             if elemento == "BUSCAR":
+                nombre = linea[0]
                 numero = QuitarComa(linea[1])
                 buscado = linea[len(linea) - 1]
-                nombre = linea[0]
                 num = ','.join(numero)
                 posiciones = Busqueda(numero, buscado)
                 if not Vacio(posiciones):
                     pos = ','.join(map(str, posiciones))
-                    print(nombre + " = " + num + " EL NUMERO " + buscado + " SE ENCUENTRA EN LA POSICION = " + pos, end="\n")
+                    print(nombre + ": = " + num + " EL NUMERO " + buscado + " SE ENCUENTRA EN LA POSICION = " + pos,end="\n")
                 else:
-                    print(nombre + " = " + num + " EL NUMERO " + buscado + " NO SE HA ENCONTRADO ", end="\n")
-def imprimir(vec):
+                    print(nombre + ": = " + num + " EL NUMERO " + buscado + " NO SE HA ENCONTRADO ", end="\n")
+def Buscar(vec):
+    for e in vec:
+        if e == "BUSCAR":
+            return True
+    return False
+def Ordenar(vec):
+    for e in vec:
+        if e == "ORDENAR":
+            return True
+    return False
+def Imprimir(vec):
     for elemento in vec:
         print(elemento)
+def Ascendente(vec):
+    for i in range(len(vec)):
+        for j in range(0, len(vec) - 1):
+            if vec[j] > vec[j + 1]:
+                temp = vec[j]
+                vec[j] = vec[j + 1]
+                vec[j + 1] = temp
+    return vec
+def Imprimirlo():
+    print("*******Todas las listas******")
+    for linea in cm:
+        o = Ordenar(linea)
+        b = Buscar(linea)
+        numeros = QuitarComa(linea[1])
+        nombre = linea[0]
+        if o is True and b is False:
+            print("---------SOLO ORDENAR---------")
+            num = ','.join(Ascendente(numeros))
+            print(nombre + ": ORDENADO = " + num, end="\n")
+        elif o is False and b is True:
+            print("---------SOLO BUSCAR---------")
+            buscado = linea[len(linea) - 1]
+            num = ','.join(numeros)
+            posiciones = Busqueda(numeros, buscado)
+            if not Vacio(posiciones):
+                pos = ','.join(map(str, posiciones))
+                print(nombre + ": = " + num + " EL NUMERO " + buscado + " SE ENCUENTRA EN LA POSICION = " + pos, end="\n")
+            else:
+                print(nombre + ": = " + num + " EL NUMERO " + buscado + " NO SE HA ENCONTRADO ", end="\n")
+        elif o is True and b is True:
+            print("-------ORDENAR Y BUSCAR-------")
+            buscado = linea[len(linea) - 1]
+            num = ','.join(Ascendente(numeros))
+            posiciones = Busqueda(numeros, buscado)
+            if not Vacio(posiciones):
+                pos = ','.join(map(str, posiciones))
+                print(nombre + ": ORDENADO = " + num + " EL NUMERO " + buscado + " SE ENCUENTRA EN LA POSICION = " + pos, end="\n")
+            else:
+                print(nombre + ": ORDENADO =" + num + " EL NUMERO " + buscado + " NO SE HA ENCONTRADO", end="\n")
+        else:
+            print("------NI BUSCAR NI ORDENAR------")
+            num = ','.join(numeros)
+            print(nombre + ": " + num, end="\n")
+def HTML():
+    lol = open('Archivo.html', 'w')
+    inicio = """<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand">
+        <img src="usacIcono.png" width="100" height="100">
+      </a>
+      <a class="navbar-brand">
+      <h2><b> Práctica 1 - Lenguajes formales y de programación </b></h2>
+      <h3><b> Allan Josué Rafael Morales - 201709196 </b></h3>
+      </a>
+    </nav>
+    </head>
+    <body>"""
+    lol.write(inicio)
+    datos = """
+    <br>
+    <br>
+    \n"""
+    lol.write(datos)
+    lol.write("""
+    <div class="container" style="text-align: center;"><h4 > <b> DATOS OPERADOS DEL ARCHIVO SELECCIONADO </b> <h4></div>
+    <br>
+    <div class="container" style="text-align: center;" > <ul class="list-group">""")
+    for linea in cm:
+        lol.write("<br>")
+        o = Ordenar(linea)
+        b = Buscar(linea)
+        numero = QuitarComa(linea[1])
+        nombreLista = linea[0]
+
+        if o is True and b is False:
+            print("ORDENADAS")
+            num = ','.join(Ascendente(numero))
+            lol.write("""<li class="list-group-item">""")
+            lol.write(" LISTA: " + nombreLista + " ORDENADA = " + num + "\n")
+            lol.write("</li>")
+        elif o is False and b is True:
+            print("BUSQUEDAS")
+            buscado = linea[len(linea) - 1]
+            num = ','.join(numero)
+            posiciones = Busqueda(numero, buscado)
+            if not Vacio(posiciones):
+                pos = ','.join(map(str, posiciones))
+                lol.write("""<li class="list-group-item">""")
+                lol.write(" LISTA: " + nombreLista + " = " + num + " EL NUMERO " + buscado + " SE ENCUENTRA EN LA POSICION = " + pos + "\n")
+                lol.write("</li>")
+            else:
+                lol.write("""<li class="list-group-item">""")
+                lol.write(" LISTA: " + nombreLista + " = " + num + " EL NUMERO " + buscado + " NO SE HA ENCONTRADO " + "\n")
+                lol.write("</li>")
+        elif o is True and b is True:
+            print("ORDENAR Y BUSCAR")
+            buscado = linea[len(linea) - 1]
+            num = ','.join(Ascendente(numero))
+            posiciones = Busqueda(numero, buscado)
+            if not Vacio(posiciones):
+                pos = ','.join(map(str, posiciones))
+                lol.write("""<li class="list-group-item">""")
+                lol.write(" LISTA: " + nombreLista + " ORDENADA = " + num + " EL NUMERO " + buscado + " SE ENCUENTRA EN LA POSICION = " + pos + "\n")
+                lol.write("</li>")
+            else:
+                lol.write("""<li class="list-group-item">""")
+                lol.write(" LISTA: " + nombreLista + " ORDENADA = " + num + " EL NUMERO " + buscado + " NO SE HA ENCONTRADO " + "\n")
+                lol.write("</li>")
+        else:
+            print("NI BUSCAR NI ORDENAR")
+            num = ','.join(numero)
+            lol.write("""<li class="list-group-item">""")
+            lol.write(nombreLista + ": " + num + "\n")
+            lol.write("</li>")
+    lol.write('\n</ul> </div> \n')
+    fin = """</body>
+        </html>"""
+    lol.write(fin)
+    lol.close()
 
 if __name__ == '__main__':
     Opcion = None
@@ -107,7 +237,7 @@ if __name__ == '__main__':
             leerDoc(root)
             print(root)
             print()
-            imprimir(cm)
+            Imprimir(cm)
             cm = QuitarLineas(cm)
         elif Opcion == 2:
             print("-------------------------------")
@@ -117,13 +247,11 @@ if __name__ == '__main__':
             Buscados()
         elif Opcion == 4:
             print("-------------------------------")
-            imptimir_todo()
-            print("-------------------------------")
+            Imprimirlo()
         elif Opcion == 5:
             print("-------------------------------")
-            desplegarHTML()
-            print("-------------------------------")
+            HTML()
         elif Opcion == 6:
-            print("-----FINALIZADO-----")
+            print("----------FINALIZADO-----------")
         else:
             print("Escoga una opcion valida")
